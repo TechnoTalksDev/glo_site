0605568d60291ff489a2c6fffda05aa9945871b6
TechnoTalksDev <technotalksyt101@gmail.com>
2024-10-24 18:13:22 -0700

    Added animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,27 @@
 <script>
 	import { WandSparkles } from 'lucide-svelte';
-	import Logo from "$lib/logo.svelte";
-	let showLogo = true
+
+	import {
+		blur,
+		fade,
+		scale,
+
+	} from 'svelte/transition';
+
+	import { onMount } from 'svelte';
+
+	let animate = false
+
+	onMount(() => {
+		animate = true
+	})
+
 </script>
 
+<input type="checkbox" bind:checked={animate} />
 
-<div id="top-blob" class="absolute top-[-15rem] left-0 right-0 ml-auto mr-auto w-[80vw] h-[20rem] rounded-[50%] bg-gradient-to-br from-surface-500 via-primary-500 to-white opacity-40 overflow-x-hidden blur-3xl" >
+{#if animate}
+<div id="top-blob" transition:blur = {{amount: 5, duration: 1500}} class="absolute top-[-15rem] left-0 right-0 ml-auto mr-auto w-[80vw] h-[20rem] rounded-[50%] bg-gradient-to-br from-surface-500 via-primary-500 to-white opacity-40 overflow-x-hidden blur-3xl" >
 </div>
 
 
@@ -15,19 +31,26 @@
 		<!--
 			<h1 class="h1 text-[3in]">GLO</h1>
 		-->
-		<span class="badge variant-ghost px-6 py-2 text-lg text-[1rem] transition-shadow duration-300 hover:shadow-xl hover:shadow-[#ffffff80] select-none"><WandSparkles class="mr-2"/>Coming Soon</span>
+		<span transition:fade = {{delay: 1000, duration: 1000}} class="badge variant-ghost px-6 py-2 text-lg text-[1rem] transition-shadow duration-300 hover:shadow-xl hover:shadow-[#ffffff80] select-none"><WandSparkles class="mr-2"/>Coming Soon</span>
 		
 
-		<Logo show = {showLogo}/>
+		<div class="relative w-auto" transition:blur = {{amount: 5, duration: 1500}}>
+			<h1 class="text-[2.75in] gradient-hero leading-normal font-[bumbbled] whitespace-nowrap select-none">
+				Glo
+			</h1>
+			<h1 class="text-[2.75in] gradient-hero leading-normal font-[bumbbled] blur-lg absolute inset-0 whitespace-nowrap select-none">
+				Glo
+			</h1>
+		</div>
 
 
 		
-		<h2 class="mt-4 tracking-tighter text-2xl bg-gradient-to-b from-white via-white to-gray-500 inline-block text-transparent bg-clip-text font-extrabold text-center relative z-20">Where your cosmetics <span class="gradient-hero">come to you</span></h2>
+		<h2 transition:fade = {{delay: 750, duration: 500}} class="mt-4 tracking-tighter text-2xl bg-gradient-to-b from-white via-white to-gray-500 inline-block text-transparent bg-clip-text font-extrabold text-center relative z-20">Where your cosmetics <span class="gradient-hero">come to you</span></h2>
 		
 		<!--
 			Inspired by carbon.host
 		-->
-		<div class="w-[40rem] relative mt-10">
+		<div class="w-[40rem] relative mt-10" transition:scale = {{delay: 750}}>
 			<div class="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-orange-500 to-transparent h-[2px] w-3/4 blur-sm"></div>
 			<div class="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-orange-500 to-transparent h-px w-3/4"></div>
 			<div class="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-red-500 to-transparent h-[5px] w-1/4 blur-sm"></div>
@@ -38,3 +61,5 @@
 	
 	</div>
 </div>
+{/if}
+
